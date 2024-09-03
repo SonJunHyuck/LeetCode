cffd0dec2ebbbee69119b511af318d59da0cee30
Sonny_Smile <56015239+SonJunHyuck@users.noreply.github.com>
2024-09-03 17:46:10 +0900

    Time: 28 ms (6.43%), Space: 16.7 MB (65.20%) - LeetHub

---
+++ 0393-utf-8-validation/0393-utf-8-validation.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    bool validUtf8(vector<int>& data) 
+    {
+        int idx = 0;
+        while(idx < data.size())
+        {
+            int numByte = 0;
+            bitset<8> b(data[idx]);
+
+            for(int i = 7; i >= 0; i--)
+            {
+                if(b[i])
+                {
+                    numByte++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            cout << b << endl;
+            
+            if(numByte == 1)
+            {
+                return false;
+            }
+
+            if(numByte > data.size())
+            {
+                return false;
+            }
+
+            // 한 문자에 4바이트
+            numByte = min(numByte, 4);
+
+            for(int i = 1; i < numByte; i++)
+            {
+                b = bitset<8>(data[idx + i]);
+
+                if(b[7] != 1 || b[6] != 0)
+                {
+                    return false;
+                }
+            }
+
+            idx += numByte == 0 ? 1 : numByte;
+        }
+
+        return true;
+    }
+};
