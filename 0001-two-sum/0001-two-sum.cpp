2004125233adc56ce4227e2c78fab11bee0e7be1
Sonny_Smile <56015239+SonJunHyuck@users.noreply.github.com>
2025-02-11 00:56:24 +0900

    Time: 4 ms (61.4%), Space: 15 MB (15.18%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) 
+    {
+        map<int, int> others;
+        vector<int> indices = {0, 0};
+
+        for(int i = 0; i < nums.size(); i++)
+        {
+            int num = nums[i];  // 2, 7
+            int other = target - num;  // 7, 2
+
+            if(others.count(other) > 0)
+            {
+                if(others[other] == i)
+                {
+                    continue;
+                }
+
+                indices[0] = others[other];
+                indices[1] = i;
+                break;
+            }
+            else
+            {
+                others.emplace(num, i);
+            }
+        }
+
+        return indices;
+    }
+};
